f902aba8b794bedaf23271ffba900db3f68dbde7
Eduard <1621251+ELynx@users.noreply.github.com>
2023-07-20 01:16:40 +0200

    Update main.ts

--- src/alpha-capture_the_flag/main.ts
+++ src/alpha-capture_the_flag/main.ts
@@ -1,5 +1,11 @@
-import { getTicks } from 'game/utils'
+import { getObjectsByPrototype } from 'game/utils';
+import { Creep } from 'game/prototypes';
+import { Flag } from 'arena/season_alpha/capture_the_flag/basic';
 
-export function loop (): void {
-  console.log('Current tick: ', getTicks())
+export function loop() {
+    var enemyFlag = getObjectsByPrototype(Flag).find(object => !object.my);
+    var myCreeps = getObjectsByPrototype(Creep).filter(object => object.my);
+    for(var creep of myCreeps) {
+        creep.moveTo(enemyFlag);
+    }
 }
