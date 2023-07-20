d06ab29dec1f05b145f4974b296a6cd493392e6b
ELynx <ELynx@users.noreply.github.com>
2023-07-20 16:25:57 +0000

    [Build Typescript] Commit built code

--- dist/alpha-capture_the_flag/main.mjs
+++ dist/alpha-capture_the_flag/main.mjs
@@ -1,12 +1,51 @@
-import { getObjectsByPrototype } from '/game/utils';
-import { Creep } from '/game/prototypes';
+import { getTicks, getObjectsByPrototype } from '/game/utils';
+import { StructureTower, Creep } from '/game/prototypes';
 import { Flag } from '/arena/season_alpha/capture_the_flag/basic';
 
+let _flagCache;
+let _towerCache;
+let _creepCache;
+function _fillCaches() {
+    if (_flagCache === undefined) {
+        _flagCache = getObjectsByPrototype(Flag);
+    }
+    if (_towerCache === undefined) {
+        _towerCache = getObjectsByPrototype(StructureTower);
+    }
+    if (_creepCache === undefined) {
+        _creepCache = getObjectsByPrototype(Creep);
+    }
+}
+class PlayerInfo {
+    constructor() {
+        this.creeps = [];
+    }
+}
+function fillPlayerInfo(whoFunction) {
+    _fillCaches();
+    const playerInfo = new PlayerInfo();
+    playerInfo.flag = _flagCache.find(x => whoFunction.apply(x));
+    const towers = _towerCache.filter(x => whoFunction.apply(x));
+    if (towers.length > 0)
+        playerInfo.tower1 = towers[0];
+    if (towers.length > 1)
+        playerInfo.tower2 = towers[1];
+    playerInfo.creeps = _creepCache.filter(x => whoFunction.apply(x));
+    return playerInfo;
+}
+let myPlayerInfo;
+let enemyPlayerInfo;
 function loop() {
-    const enemyFlag = getObjectsByPrototype(Flag).find(object => !object.my);
-    const myCreeps = getObjectsByPrototype(Creep).filter(object => object.my);
-    for (const creep of myCreeps) {
-        creep.moveTo(enemyFlag);
+    if (getTicks() === 1) {
+        myPlayerInfo = fillPlayerInfo(function my(what) {
+            return what.my === true;
+        });
+        enemyPlayerInfo = fillPlayerInfo(function enemy(what) {
+            return what.my === false;
+        });
+    }
+    for (const creep of myPlayerInfo.creeps) {
+        creep.moveTo(enemyPlayerInfo.flag);
     }
 }
 
