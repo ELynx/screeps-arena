98f7e1f39513e73ef3a39c4c86903efde2266284
Eduard <1621251+ELynx@users.noreply.github.com>
2023-07-20 18:14:09 +0200

    magic

--- src/alpha-capture_the_flag/main.ts
+++ src/alpha-capture_the_flag/main.ts
@@ -1,11 +1,69 @@
-import { getObjectsByPrototype } from 'game/utils'
-import { Creep, Position } from 'game/prototypes'
+import { getObjectsByPrototype, getTicks } from 'game/utils'
+import { Creep, Position, StructureTower } from 'game/prototypes'
 import { Flag } from 'arena/season_alpha/capture_the_flag/basic'
 
+let _flagCache: Flag[]
+let _towerCache: StructureTower[]
+let _creepCache: Creep[]
+
+function _fillCaches () {
+  if (_flagCache === undefined) {
+    _flagCache = getObjectsByPrototype(Flag)
+  }
+
+  if (_towerCache === undefined) {
+    _towerCache = getObjectsByPrototype(StructureTower)
+  }
+
+  if (_creepCache === undefined) {
+    _creepCache = getObjectsByPrototype(Creep)
+  }
+}
+
+class PlayerInfo {
+  flag: Flag | undefined
+
+  tower1: StructureTower | undefined
+  tower2: StructureTower | undefined
+
+  creeps: Creep[]
+}
+
+function fillPlayerInfo (whoFunction: Function) : PlayerInfo {
+  _fillCaches()
+
+  let playerInfo = new PlayerInfo
+
+  playerInfo.flag = _flagCache.find(x => whoFunction.apply(x))
+  
+  let towers = _towerCache.filter(x => whoFunction.apply(x))
+  if (towers.length > 0) playerInfo.tower1 = towers[0]
+  if (towers.length > 1) playerInfo.tower2 = towers[1]
+
+  playerInfo.creeps = _creepCache.filter(x => whoFunction.apply(x))
+
+  return playerInfo
+}
+
+let myPlayerInfo : PlayerInfo
+let enemyPlayerInfo : PlayerInfo
+
 export function loop () {
-  const enemyFlag = getObjectsByPrototype(Flag).find(object => !object.my)
-  const myCreeps = getObjectsByPrototype(Creep).filter(object => object.my)
-  for (const creep of myCreeps) {
-    creep.moveTo(enemyFlag as Position)
+  if (getTicks() === 1) {
+    myPlayerInfo = fillPlayerInfo(
+      function my(what: Flag | StructureTower | Creep) : boolean {
+        return what.my
+      }
+    )
+
+    enemyPlayerInfo = fillPlayerInfo(
+      function enemy(what: Flag | StructureTower | Creep) : boolean {
+        return !what.my
+      }
+    )
+  }
+
+  for (const creep of myPlayerInfo.creeps) {
+    creep.moveTo(enemyPlayerInfo.flag as Position)
   }
 }
