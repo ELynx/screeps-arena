46216127e6054eb19c114b44e421abb704aaa817
ELynx <1621251+ELynx@users.noreply.github.com>
2023-07-21 16:17:03 +0200

    Update main.ts

--- src/alpha-capture_the_flag/main.ts
+++ src/alpha-capture_the_flag/main.ts
@@ -92,11 +92,11 @@ export function loop (): void {
       }
     }
 
-	if (myPlayerInfo.flag && enemyPlayerInfo.flag) {
+    if (myPlayerInfo.flag && enemyPlayerInfo.flag) {
       engageDistance = getRange(myPlayerInfo.flag as Position, enemyPlayerInfo.flag as Position)
-	} else {
+    } else {
 	  engageDistance = TOWER_RANGE * 2
-	}
+    }
   }
 
   play()
