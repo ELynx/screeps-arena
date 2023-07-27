3364ef7faff40506bb2778cf00420d0a1836a8b5
Eduard <1621251+ELynx@users.noreply.github.com>
2023-07-28 00:59:52 +0200

    Update main.ts

--- src/alpha-capture_the_flag/main.ts
+++ src/alpha-capture_the_flag/main.ts
@@ -1,4 +1,4 @@
-import * as gridAssign from 'grid-assign-js'
+import { assign } from 'grid-assign-js'
 
 import { Creep, CreepMoveResult, GameObject, OwnedStructure, Position, Structure, StructureTower } from 'game/prototypes'
 import { OK, ATTACK, HEAL, MOVE, RANGED_ATTACK, RANGED_ATTACK_DISTANCE_RATE, RANGED_ATTACK_POWER, RESOURCE_ENERGY, TOWER_ENERGY_COST, TOWER_FALLOFF, TOWER_FALLOFF_RANGE, TOWER_OPTIMAL_RANGE, TOWER_RANGE, ERR_NO_BODYPART, ERR_TIRED, ERR_INVALID_ARGS } from 'game/constants'
@@ -736,7 +736,7 @@ class BodyPartGoal implements Goal {
       [-1, 1]
     ]
 
-    const assignments = gridAssign.assign({
+    const assignments = assign({
       points: peopleCallingTaxiLocations,
       assignTo: taxiDriverLocations
     })
