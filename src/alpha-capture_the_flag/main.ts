49c75e4092e44fd4a6c77af5ec83f90c13c179a7
Eduard <1621251+ELynx@users.noreply.github.com>
2023-07-30 01:43:45 +0200

    lint

--- src/alpha-capture_the_flag/main.ts
+++ src/alpha-capture_the_flag/main.ts
@@ -4,7 +4,6 @@ import { BodyPartType, Creep, CreepAttackResult, CreepHealResult, CreepMoveResul
 import { OK, ATTACK, HEAL, MOVE, RANGED_ATTACK, RANGED_ATTACK_DISTANCE_RATE, RANGED_ATTACK_POWER, RESOURCE_ENERGY, TOWER_ENERGY_COST, TOWER_FALLOFF, TOWER_FALLOFF_RANGE, TOWER_OPTIMAL_RANGE, TOWER_RANGE, ERR_NO_BODYPART, ERR_TIRED, ERR_INVALID_ARGS, ERR_NOT_IN_RANGE, TOUGH } from 'game/constants'
 import { Direction, FindPathOptions, getCpuTime, getDirection, getObjectsByPrototype, getRange, getTicks } from 'game/utils'
 import { Color, LineVisualStyle, Visual } from 'game/visual'
-import { searchPath } from 'game/path-finder'
 import { BodyPart, Flag } from 'arena/season_alpha/capture_the_flag/basic'
 
 // custom demands to navigation
